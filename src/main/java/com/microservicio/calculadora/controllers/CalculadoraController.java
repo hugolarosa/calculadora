a00e4ed86a14e6141bd6b979d19bc3b25b5f027d
hugolarosa <hugolarosa@hotmail.com>
2023-03-01 01:27:13 +0100

    add CalculadoraController

--- src/main/java/com/microservicio/calculadora/controllers/CalculadoraController.java
+++ src/main/java/com/microservicio/calculadora/controllers/CalculadoraController.java
@@ -1,12 +1,27 @@
 package com.microservicio.calculadora.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.microservicio.calculadora.services.ICalculadoraServicio;
 
 @RestController
 @RequestMapping("/api")
 public class CalculadoraController {
 	
-	
+	@Autowired
+	ICalculadoraServicio calculadoraServicio;
+
+	@GetMapping("/calcula")
+	public Long calcula(@RequestParam(name = "primerNumero") Long primerNumero,
+			@RequestParam(name = "segundoNumero") Long segundoNumero,
+			@RequestParam(name = "operacion") String operacion) {
+		
+		return calculadoraServicio.calcula(primerNumero, segundoNumero, operacion);
+		
+	}
 	
 }
--- src/main/java/com/microservicio/calculadora/services/CalculadoraServicio.java
+++ src/main/java/com/microservicio/calculadora/services/CalculadoraServicio.java
@@ -8,10 +8,10 @@ public class CalculadoraServicio implements ICalculadoraServicio {
 	@Override
 	public Long calcula(Long primerNumero, Long segundoNumero, String operacion) {
 		switch(operacion){
-			case "+":
+			case "suma":
 				return primerNumero + segundoNumero;
-			case "-":
-				return primerNumero + segundoNumero;
+			case "resta":
+				return primerNumero - segundoNumero;
 				default:
 					throw new RuntimeException("Operacion no existente");
 		}
